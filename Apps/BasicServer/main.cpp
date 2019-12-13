354ba573a94377d6f4fbafbcfb8d0a05a3cf4e20
sergey radionov <rsatom@gmail.com>
2019-12-13 23:39:21 +0700

    Basic server implementation started

---
+++ Apps/BasicServer/main.cpp
@@ -0,0 +1,34 @@
+#include <CxxPtr/GlibPtr.h>
+
+#include "Signalling/WsServer.h"
+#include "Signalling/ServerSession.h"
+
+
+static std::unique_ptr<rtsp::ServerSession> CreateSession (
+    const std::function<void (const rtsp::Request*)>& sendRequest,
+    const std::function<void (const rtsp::Response*)>& sendResponse) noexcept
+{
+    return std::make_unique<ServerSession>(sendRequest, sendResponse);
+}
+
+int main(int argc, char *argv[])
+{
+    enum {
+        SERVER_PORT = 8081,
+    };
+
+    signalling::Config config {};
+    config.port = SERVER_PORT;
+
+    GMainLoopPtr loopPtr(g_main_loop_new(nullptr, FALSE));
+    GMainLoop* loop = loopPtr.get();
+
+    signalling::WsServer server(config, loop, CreateSession);
+
+    if(server.init())
+        g_main_loop_run(loop);
+    else
+        return -1;
+
+    return 0;
+}
