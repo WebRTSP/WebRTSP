b0ba3b784ba4fe4f514f391541d35e3ab4d4eddb
sergey radionov <rsatom@gmail.com>
2020-01-03 12:20:13 +0700

    don't duplicate code

--- Apps/InverseProxy/InverseProxyServerApp/InverseProxyTest.cpp
+++ Apps/InverseProxy/InverseProxyServerApp/InverseProxyTest.cpp
@@ -4,51 +4,13 @@
 
 #include "CxxPtr/libconfigDestroy.h"
 
+#include "Common/ConfigHelpers.h"
 #include "../InverseProxyServer/Log.h"
 #include "../InverseProxyServer/InverseProxyServer.h"
 
 
 static const auto Log = InverseProxyServerLog;
 
-
-static std::deque<std::string> ConfigDirs()
-{
-    std::deque<std::string> dirs;
-
-#ifdef SNAPCRAFT_BUILD
-    if(const gchar* data = g_getenv("SNAP"))
-        dirs.push_back(std::string(data) + "/etc");
-
-    if(const gchar* common = g_getenv("SNAP_COMMON"))
-        dirs.push_back(common);
-#else
-    const gchar * const *systemConfigDirs = g_get_system_config_dirs();
-    while(*systemConfigDirs) {
-        dirs.push_back(*systemConfigDirs);
-        ++systemConfigDirs;
-    }
-
-    const gchar* configDir = g_get_user_config_dir();
-    if(configDir)
-        dirs.push_back(configDir);
-#endif
-
-    return dirs;
-}
-
-static std::string FullPath(const std::string& configDir, const std::string& path)
-{
-    std::string fullPath;
-    if(!g_path_is_absolute(path.c_str())) {
-        gchar* tmpPath =
-            g_build_filename(configDir.c_str(), path.c_str(), NULL);
-        fullPath = tmpPath;
-        g_free(tmpPath);
-    } else
-        fullPath = path;
-
-    return fullPath;
-}
 
 static bool LoadConfig(InverseProxyServerConfig* config)
 {
