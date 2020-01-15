99dc14695d651a926d996c1fc5c944882a54ae02
sergey radionov <rsatom@gmail.com>
2020-01-15 21:01:36 +0700

    InverseProxyServer: config moved to separate file

---
+++ Apps/InverseProxy/InverseProxyServer/InverseProxyServerConfig.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+#include <spdlog/common.h>
+
+
+typedef std::map<const std::string, const std::string> AuthTokens;
+
+struct InverseProxyServerConfig
+{
+    spdlog::level::level_enum logLevel = spdlog::level::info;
+
+    std::string serverName;
+    std::string certificate;
+    std::string key;
+
+    unsigned short frontPort;
+    unsigned short secureFrontPort;
+
+    unsigned short backPort;
+    unsigned short secureBackPort;
+
+    std::string turnServer;
+    std::string turnAuthSecret;
+
+    AuthTokens backAuthTokens;
+};
