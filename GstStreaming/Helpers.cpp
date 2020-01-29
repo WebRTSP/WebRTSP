e6685e3552199190707429cfa0e67103be397da4
sergey radionov <rsatom@gmail.com>
2020-01-29 19:27:25 +0700

    GstStreaming: add-turn-server signal is not available in gstreamer v1.14
    which come by default with Ubuntu 18.04 LTS

--- GstStreaming/Helpers.cpp
+++ GstStreaming/Helpers.cpp
@@ -1,6 +1,7 @@
 #include "Helpers.h"
 
 #include <gst/gst.h>
+#include <gst/pbutils/pbutils.h>
 
 
 namespace GstStreaming
@@ -22,6 +23,12 @@ void SetIceServers(
     GstElement* rtcbin,
     const std::deque<std::string>& iceServers)
 {
+    guint vMajor = 0, vMinor = 0;
+    gst_plugins_base_version(&vMajor, &vMinor, nullptr, nullptr);
+
+    const bool useAddTurnServer =
+        vMajor > 1 || (vMajor == 1 && vMinor >= 16);
+
     for(const std::string& iceServer: iceServers) {
         using namespace GstStreaming;
         switch(ParseIceServerType(iceServer)) {
@@ -33,11 +40,18 @@ void SetIceServers(
                 break;
             case IceServerType::Turn:
             case IceServerType::Turns: {
-                gboolean ret;
-                g_signal_emit_by_name(
-                    rtcbin,
-                    "add-turn-server", iceServer.c_str(),
-                    &ret);
+                if(useAddTurnServer) {
+                    gboolean ret;
+                    g_signal_emit_by_name(
+                        rtcbin,
+                        "add-turn-server", iceServer.c_str(),
+                        &ret);
+                } else {
+                    g_object_set(
+                        rtcbin,
+                        "turn-server", iceServer.c_str(),
+                        nullptr);
+                }
                 break;
             }
             default:
