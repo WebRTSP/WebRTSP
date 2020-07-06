f7faa51543f3a6da2347f181beafee307bb48523
sergey radionov <rsatom@gmail.com>
2020-07-06 19:11:00 +0700

    BasicServer: added basic http server

--- Apps/BasicServer/main.cpp
+++ Apps/BasicServer/main.cpp
@@ -1,9 +1,15 @@
 #include <memory>
 
 #include <CxxPtr/GlibPtr.h>
+#include <CxxPtr/libwebsocketsPtr.h>
 
+#include "Common/LwsLog.h"
+#include "Http/Log.h"
+#include "Http/HttpServer.h"
+#include "Signalling/Log.h"
 #include "Signalling/WsServer.h"
 #include "Signalling/ServerSession.h"
+#include "GstStreaming/LibGst.h"
 #include "GstStreaming/GstTestStreamer.h"
 
 
@@ -21,19 +27,30 @@ static std::unique_ptr<rtsp::ServerSession> CreateSession (
 
 int main(int argc, char *argv[])
 {
-    enum {
-        SERVER_PORT = 8081,
-    };
+    LibGst libGst;
 
+    http::Config httpConfig {};
     signalling::Config config {};
-    config.port = SERVER_PORT;
 
     GMainLoopPtr loopPtr(g_main_loop_new(nullptr, FALSE));
     GMainLoop* loop = loopPtr.get();
 
+    lws_context_creation_info lwsInfo {};
+    lwsInfo.gid = -1;
+    lwsInfo.uid = -1;
+    lwsInfo.options = LWS_SERVER_OPTION_EXPLICIT_VHOSTS;
+#if defined(LWS_WITH_GLIB)
+    lwsInfo.options |= LWS_SERVER_OPTION_GLIB;
+    lwsInfo.foreign_loops = reinterpret_cast<void**>(&loop);
+#endif
+
+    LwsContextPtr contextPtr(lws_create_context(&lwsInfo));
+    lws_context* context = contextPtr.get();
+
+    http::Server httpServer(httpConfig, loop);
     signalling::WsServer server(config, loop, CreateSession);
 
-    if(server.init())
+    if(httpServer.init(context) && server.init(context))
         g_main_loop_run(loop);
     else
         return -1;
