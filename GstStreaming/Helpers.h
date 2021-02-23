405f302e2e336ec75ddd4b3dce1e53e8f5d7cb36
sergey radionov <rsatom@gmail.com>
2021-02-23 11:56:36 +0700

    GstStreaming: Types.h introduced

--- GstStreaming/Helpers.h
+++ GstStreaming/Helpers.h
@@ -5,16 +5,11 @@
 
 #include <gst/gst.h>
 
+#include "Types.h"
+
 
 namespace GstStreaming
 {
-
-enum class IceServerType {
-    Unknown,
-    Stun,
-    Turn,
-    Turns,
-};
 
 IceServerType ParseIceServerType(const std::string& iceServer);
 void SetIceServers(
---
+++ GstStreaming/Types.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace GstStreaming
+{
+
+enum class IceServerType {
+    Unknown,
+    Stun,
+    Turn,
+    Turns,
+};
+
+}
