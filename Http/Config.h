41072688926f266328411d571b5592d583362cc7
sergey radionov <rsatom@gmail.com>
2023-11-02 14:16:31 +0700

    Http: it doesn't support TLS actually...

--- Http/Config.h
+++ Http/Config.h
@@ -8,14 +8,8 @@ namespace http {
 
 struct Config
 {
-    std::string serverName;
-    std::string certificate;
-    std::string key;
-
     bool bindToLoopbackOnly = true;
     unsigned short port = 5080;
-    bool secureBindToLoopbackOnly = false;
-    unsigned short securePort = 5443;
 
     std::string wwwRoot = "./www";
 
