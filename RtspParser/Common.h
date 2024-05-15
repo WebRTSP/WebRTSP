b17ed76dfd610d9da15f4a872b0b19f4546173ac
sergey radionov <rsatom@gmail.com>
2024-05-15 12:52:29 +0700

    Common: Some useful constants added

--- RtspParser/Common.h
+++ RtspParser/Common.h
@@ -10,6 +10,10 @@ typedef std::string MediaSessionId;
 
 const char UriSeparator = '/';
 const char *const WildcardUri = "*";
+
+const char *const ContentTypeFieldName = "Content-Type";
 const char *const TextParametersContentType = "text/parameters";
+const char *const SdpContentType = "application/sdp";
+const char *const IceCandidateContentType = "application/x-ice-candidate";
 
 }
