7421b6fd217dec2bf241a5cedbfccdf0fe383aad
sergey radionov <rsatom@gmail.com>
2024-07-05 22:40:50 +0700

    RtspParser: TextListContentType added

--- RtspParser/Common.h
+++ RtspParser/Common.h
@@ -12,6 +12,7 @@ const char UriSeparator = '/';
 const char *const WildcardUri = "*";
 
 const char *const ContentTypeFieldName = "Content-Type";
+const char *const TextListContentType = "text/list";
 const char *const TextParametersContentType = "text/parameters";
 const char *const SdpContentType = "application/sdp";
 const char *const IceCandidateContentType = "application/x-ice-candidate";
