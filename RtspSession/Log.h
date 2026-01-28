c96cb31092d76cc6fbe552ae2464bd7ced1362e9
sergey radionov <rsatom@gmail.com>
2026-01-28 22:42:47 +0700

    [refactoring]

--- RtspSession/Log.h
+++ RtspSession/Log.h
@@ -8,3 +8,11 @@
 void InitRtspSessionLogger(spdlog::level::level_enum);
 const std::shared_ptr<spdlog::logger>& RtspSessionLog();
 std::shared_ptr<spdlog::logger> MakeRtspSessionLogger(const std::string& context);
+
+void InitServerSessionLogger(spdlog::level::level_enum level);
+std::shared_ptr<spdlog::logger> MakeServerSessionLogger(const std::string& context);
+
+void InitClientSessionLogger(spdlog::level::level_enum level);
+const std::shared_ptr<spdlog::logger>& ClientSessionLog();
+std::shared_ptr<spdlog::logger> MakeClientSessionLogger(const std::string& context);
+
