c1f719100fcffde2fa1b4f1f5de60698832fc1db
sergey radionov <rsatom@gmail.com>
2026-02-16 16:05:14 +0700

    QActor helper added
    as alternative to Helpers/Actor
    but with Qt signals/slots support

---
+++ Qt/QActor.cpp
@@ -0,0 +1,142 @@
+#include "QActor.h"
+
+#include <QThread>
+
+#include <mutex>
+#include <condition_variable>
+
+#include "Helpers/EventSource.h"
+
+
+namespace {
+
+struct GlibUnref {
+    void operator() (GMainContext* context)
+        { g_main_context_unref(context); }
+    void operator() (GMainLoop* loop)
+        { g_main_loop_unref(loop); }
+    void operator() (GAsyncQueue* queue)
+        { g_async_queue_unref(queue); }
+};
+
+typedef
+    std::unique_ptr<
+        GMainContext,
+        GlibUnref> GMainContextPtr;
+typedef
+    std::unique_ptr<
+        GMainLoop,
+        GlibUnref> GMainLoopPtr;
+typedef
+    std::unique_ptr<
+        GAsyncQueue,
+        GlibUnref> GAsyncQueuePtr;
+
+struct Action {
+    QActor::Action action;
+};
+
+void OnEvent(GAsyncQueue* queue)
+{
+    while(gpointer item = g_async_queue_try_pop(queue)) {
+        std::unique_ptr<Action>(static_cast<Action*>(item))->action();
+    }
+}
+
+void ActorMain(
+    GMainContext* mainContext,
+    GMainLoop* mainLoop,
+    GAsyncQueue* queue,
+    EventSource* notifier)
+{
+    g_main_context_push_thread_default(mainContext);
+
+    notifier->subscribe(std::bind(&OnEvent, queue));
+
+    g_main_loop_run(mainLoop);
+
+    g_main_context_pop_thread_default(mainContext);
+}
+
+}
+
+struct QActor::Private {
+    Private();
+
+    GMainContextPtr mainContextPtr;
+    GMainLoopPtr mainLoopPtr;
+    GAsyncQueuePtr queuePtr;
+    EventSource notifier;
+    QThread *const actorThread;
+};
+
+QActor::Private::Private() :
+    mainContextPtr(g_main_context_new()),
+    mainLoopPtr(g_main_loop_new(mainContextPtr.get(), FALSE)),
+    queuePtr(g_async_queue_new()),
+    notifier(mainContextPtr.get()),
+    actorThread(
+        QThread::create(
+            ActorMain,
+            mainContextPtr.get(),
+            mainLoopPtr.get(),
+            queuePtr.get(),
+            &notifier))
+{
+}
+
+QActor::QActor(QObject* parent) :
+    QObject(parent),
+    _p(std::make_unique<Private>())
+{
+    _p->actorThread->start();
+}
+
+QActor::~QActor()
+{
+    if(_p->actorThread->isRunning()) {
+        postAction([loop = _p->mainLoopPtr.get()] () {
+            g_main_loop_quit(loop);
+        });
+        _p->actorThread->wait();
+    }
+}
+
+void QActor::postAction(const Action& action)
+{
+    g_async_queue_push(
+        _p->queuePtr.get(),
+        new ::Action { action });
+    _p->notifier.postEvent();
+}
+
+void QActor::postAction(Action&& action)
+{
+    g_async_queue_push(
+        _p->queuePtr.get(),
+        new ::Action { std::move(action) });
+    _p->notifier.postEvent();
+}
+
+void QActor::sendAction(const Action& action)
+{
+    std::mutex guard;
+    std::condition_variable conditional;
+    bool handled = false;
+
+    std::unique_lock guardLock(guard);
+
+    g_async_queue_push(
+        _p->queuePtr.get(),
+        new ::Action {
+            [&guard, &conditional, &action, &handled] () {
+                std::unique_lock guardLock(guard);
+                action();
+                handled = true;
+                conditional.notify_one();
+            }
+        });
+    _p->notifier.postEvent();
+
+    conditional.wait(guardLock, [&handled] () { return handled; });
+}
---
+++ Qt/QActor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+
+#include <QObject>
+
+
+class QActor: public QObject
+{
+    Q_OBJECT
+
+    QActor(QActor&) = delete;
+    QActor& operator = (QActor&) = delete;
+
+public:
+    QActor(QObject* parent = nullptr);
+    ~QActor();
+
+    typedef std::function<void ()> Action;
+    void postAction(const Action&);
+    void postAction(Action&&);
+    void sendAction(const Action&);
+
+private:
+    struct Private;
+    std::unique_ptr<Private> _p;
+};
